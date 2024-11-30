873885271c46163ccff3500b6ecf5393a90d6a56
Sandra Misigo <misigosandra@gmail.com>
2024-11-30 13:51:26 +0300

    myscopejava1lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName = 'bob' 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+    console.log(customerName.toUpperCase());
+
+    //setting bestCustomer to 'not bob'
+var bestCustomer;
+var customerName ='bob';
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+
+//overwriteBestCustomer to equal maybe customer
+ var newbestCustomer;
+function overwriteBestCustomer(newBestCustomer){
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'june';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'mitchelle';
+    console.log(leastFavoriteCustomer) 
+}
+console.log(changeLeastFavoriteCustomer())
